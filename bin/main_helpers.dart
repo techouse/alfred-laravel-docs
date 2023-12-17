79d49da5a41f1a9ee2fb652f5a2e9ab2d3750ce5
Klemen Tušar <techouse@gmail.com>
2023-12-17 16:01:20 +0000

    :alien: replace dart_algolia with algoliasearch

--- bin/main_helpers.dart
+++ bin/main_helpers.dart
@@ -31,54 +31,62 @@ void _showPlaceholder() {
 }
 
 Future<void> _performSearch(String query, {String? version}) async {
-  final AlgoliaQuerySnapshot snapshot = await AlgoliaSearch.query(
-    query,
-    version: version,
-  );
-
-  if (snapshot.nbHits > 0) {
-    final AlfredItems items = AlfredItems(
-      snapshot.hits.map((snapshot) => SearchResult.fromJson(snapshot.data)).map(
-        (result) {
-          final String title = _unescape.convert(result.hierarchy.last);
-          final Map<String, String?> hierarchy = result.hierarchy.toJson()
-            ..removeWhere((_, value) => value == null || value == title);
-
-          return AlfredItem(
-            uid: result.objectID,
-            title: title,
-            subtitle: hierarchy.length > 0
-                ? _unescape.convert(hierarchy.values.join(' > ')).truncate(75)
-                : '',
-            arg: result.url,
-            text: AlfredItemText(
-              largeType: title,
-              copy: result.url,
-            ),
-            quickLookUrl: result.url,
-            icon: AlfredItemIcon(path: 'icon.png'),
-            valid: true,
-          );
-        },
-      ).toList(),
+  try {
+    final SearchResponse searchResponse = await AlgoliaSearch.query(
+      query,
+      version: version,
     );
-    _workflow.addItems(items.items);
-  } else {
-    final Uri url =
-        Uri.https('www.google.com', '/search', {'q': 'Laravel $query'});
 
-    _workflow.addItem(
-      AlfredItem(
-        title: 'No matching answers found',
-        subtitle: 'Shall I try and search Google?',
-        arg: url.toString(),
-        text: AlfredItemText(
-          copy: url.toString(),
+    if (searchResponse.nbHits > 0) {
+      final AlfredItems items = AlfredItems(
+        searchResponse.hits
+            .map((Hit hit) =>
+            SearchResult.fromJson(
+                <String, dynamic>{...hit, 'objectID': hit.objectID}))
+            .map(
+              (SearchResult result) {
+            final String title = _unescape.convert(result.hierarchy.last);
+            final Map<String, String?> hierarchy = result.hierarchy.toJson()
+              ..removeWhere((_, value) => value == null || value == title);
+
+            return AlfredItem(
+              uid: result.objectID,
+              title: title,
+              subtitle: hierarchy.isNotEmpty
+                  ? _unescape.convert(hierarchy.values.join(' > ')).truncate(75)
+                  : '',
+              arg: result.url,
+              text: AlfredItemText(
+                largeType: title,
+                copy: result.url,
+              ),
+              quickLookUrl: result.url,
+              icon: AlfredItemIcon(path: 'icon.png'),
+              valid: true,
+            );
+          },
+        ).toList(),
+      );
+      _workflow.addItems(items.items);
+    } else {
+      final Uri url =
+      Uri.https('www.google.com', '/search', {'q': 'Laravel $query'});
+
+      _workflow.addItem(
+        AlfredItem(
+          title: 'No matching answers found',
+          subtitle: 'Shall I try and search Google?',
+          arg: url.toString(),
+          text: AlfredItemText(
+            copy: url.toString(),
+          ),
+          quickLookUrl: url.toString(),
+          icon: AlfredItemIcon(path: 'google.png'),
+          valid: true,
         ),
-        quickLookUrl: url.toString(),
-        icon: AlfredItemIcon(path: 'google.png'),
-        valid: true,
-      ),
-    );
+      );
+    }
+  } finally {
+    AlgoliaSearch.dispose();
   }
 }
